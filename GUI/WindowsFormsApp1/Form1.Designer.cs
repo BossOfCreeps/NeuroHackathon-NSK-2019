1831f3e04ef2311da25d059867cd65e035886e92
BossOfCreeps <seva-ra4is@mail.ru>
2019-12-09 11:17:09 +0700

    Add files via upload

---
+++ GUI/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(226, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Стихотворение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button2.Location = new System.Drawing.Point(12, 88);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(226, 70);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Песня";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button3.Location = new System.Drawing.Point(12, 164);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(226, 70);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Мультик";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button4.Location = new System.Drawing.Point(12, 240);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(226, 70);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Зарядка";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button5.Location = new System.Drawing.Point(12, 316);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(226, 70);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Загадки";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button6.Location = new System.Drawing.Point(319, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(203, 179);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button7.Location = new System.Drawing.Point(528, 12);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(203, 179);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button8.Location = new System.Drawing.Point(319, 197);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(203, 179);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button9.Location = new System.Drawing.Point(528, 197);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(203, 179);
+            this.button9.TabIndex = 7;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(765, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 100);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "КГР";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(765, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 100);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Пульс";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(904, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 100);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "ЭЭГ";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.Control;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(904, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 100);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Эмоция";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1058, 407);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ GUI/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Thread t1, t2, t3, t4;
+        IPEndPoint ipPoint;
+        Socket listenSocket, handler;
+
+        string str1 = "КГР";
+        string str2 = "Пульс";
+        string str3 = "";
+        string str4 = "ЭЭГ";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            t1 = new Thread(KRG);
+            t2 = new Thread(Pulse);
+            t3 = new Thread(EEG);
+            t4 = new Thread(Emotion);
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t4.Start();
+
+            ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8005);
+            listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listenSocket.Bind(ipPoint);
+            listenSocket.Listen(10);
+            // Process.Start(@"C:\Users\Lab210\3D Objects\hackaton\Новая папка\webCamEmocognizer-masterFinal\DetectEmotion.py");
+        }
+
+        void KRG()
+        {
+            IPEndPoint ipPoint1 = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8006);
+            Socket listenSocket1 = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listenSocket1.Bind(ipPoint1);
+            listenSocket1.Listen(10);
+            while (true)
+            {
+                Socket handler1 = listenSocket1.Accept();
+                // получаем сообщение
+                StringBuilder builder1 = new StringBuilder();
+                int bytes1 = 0; // количество полученных байтов
+                byte[] data1 = new byte[256]; // буфер для получаемых данных
+
+                do
+                {
+                    bytes1 = handler1.Receive(data1);
+                    builder1.Append(Encoding.UTF8.GetString(data1, 0, bytes1));
+                }
+                while (handler1.Available > 0);
+
+                str1 = builder1.ToString();
+            }
+        }
+
+        void Pulse()
+        {
+            IPEndPoint ipPoint3 = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8007);
+            Socket listenSocket3 = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listenSocket3.Bind(ipPoint3);
+            listenSocket3.Listen(10);
+            while (true)
+            {
+                Socket handler3 = listenSocket3.Accept();
+                // получаем сообщение
+                StringBuilder builder3 = new StringBuilder();
+                int bytes3 = 0; // количество полученных байтов
+                byte[] data3 = new byte[256]; // буфер для получаемых данных
+
+                do
+                {
+                    bytes3 = handler3.Receive(data3);
+                    builder3.Append(Encoding.UTF8.GetString(data3, 0, bytes3));
+                }
+                while (handler3.Available > 0);
+
+                str2 = builder3.ToString();
+            }
+        }
+
+        void EEG()
+        {
+            IPEndPoint ipPoint2 = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8009);
+            Socket listenSocket2 = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listenSocket2.Bind(ipPoint2);
+            listenSocket2.Listen(10);
+            while (true)
+            {
+                Socket handler2 = listenSocket2.Accept();
+                // получаем сообщение
+                StringBuilder builder2 = new StringBuilder();
+                int bytes2 = 0; // количество полученных байтов
+                byte[] data2 = new byte[256]; // буфер для получаемых данных
+
+                do
+                {
+                    bytes2 = handler2.Receive(data2);
+                    builder2.Append(Encoding.UTF8.GetString(data2, 0, bytes2));
+                }
+                while (handler2.Available > 0);
+
+                str4 = builder2.ToString();
+            }
+        }
+
+        void Emotion()
+        {
+            IPEndPoint ipPoint4 = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8008);
+            Socket listenSocket4 = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listenSocket4.Bind(ipPoint4);
+            listenSocket4.Listen(10);
+            while (true)
+            {
+                Socket handler4 = listenSocket4.Accept();
+                // получаем сообщение
+                StringBuilder builder4 = new StringBuilder();
+                int bytes4 = 0; // количество полученных байтов
+                byte[] data4 = new byte[256]; // буфер для получаемых данных
+
+                do
+                {
+                    bytes4 = handler4.Receive(data4);
+                    builder4.Append(Encoding.UTF8.GetString(data4, 0, bytes4));
+                }
+                while (handler4.Available > 0);
+
+                str3 = builder4.ToString();
+            }
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            button6.Visible = false;
+            button7.Visible = false;
+            button8.Visible = false;
+            button9.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            send("1");
+            button6.Visible = true; button7.Visible = true; button8.Visible = true; button9.Visible = true;
+            button6.Text = "Хороший"; button7.Text = "В горошек"; button8.Text = "Проросший"; button9.Text = "Засохший";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            send("2");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            send("3");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            send("4");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            send("5");
+            button6.Visible = true; button7.Visible = true; button8.Visible = true; button9.Visible = true;
+            button6.Text = "Рыбы"; button7.Text = "Корабли"; button8.Text = "Моряки"; button9.Text = "Водоросли";
+        }
+
+
+
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (button6.Text == "Хороший")
+                send("6");
+            else
+                send("A");
+
+            button6.Visible = false;
+            button7.Visible = false;
+            button8.Visible = false;
+            button9.Visible = false;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (button7.Text == "В горошек")
+                send("7");
+            else
+                send("B");
+
+            button6.Visible = false;
+            button7.Visible = false;
+            button8.Visible = false;
+            button9.Visible = false;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (button8.Text == "Проросший")
+                send("8");
+            else
+                send("C");
+            button6.Visible = false;
+            button7.Visible = false;
+            button8.Visible = false;
+            button9.Visible = false;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (button8.Text == "Засохший")
+                send("9");
+            else
+                send("D");
+            button6.Visible = false;
+            button7.Visible = false;
+            button8.Visible = false;
+            button9.Visible = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (str2.Length == 6)
+            {
+                label1.Text = "КГР: " + str2.Substring(0, 3);// Substring str2[0..2];
+                if (Convert.ToInt32(str2.Substring(0, 3)) < 280)
+                {
+                    label1.BackColor = Color.Red;
+                }
+                else
+                {
+                    if (Convert.ToInt32(str2.Substring(0, 3)) > 320)
+                    {
+                        label1.BackColor = Color.Red;
+                    }
+                    else
+                    {
+                        label1.BackColor = Color.Green;
+                    }
+                }
+
+                label2.Text = "Пульс: " + str2.Substring(3, 3);
+            }
+            switch (str3)
+            {
+                case "[3]":
+                    label3.Text = "Эмоция: Happy!!";
+                    break;
+                case "[0]":
+                    label3.Text = "Эмоция: Angry";
+                    break;
+                case "[1]":
+                    label3.Text = "Эмоция: Disgust";
+                    break;
+                case "[2]":
+                    label3.Text = "Эмоция: Fear";
+                    break;
+                case "[4]":
+                    label3.Text = "Эмоция: Sad";
+                    break;
+                case "[5]":
+                    label3.Text = "Эмоция: Surprise";
+                    break;
+                default:
+                    label3.Text = "Эмоция";
+                    break;
+            }
+            label4.Text = str4;
+            if (str4 == "0") 
+                {
+                label4.BackColor = Color.Red;
+                }
+                else
+                {
+                label4.BackColor = Color.Green;
+                }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        void send(string s)
+        {
+            handler = listenSocket.Accept();
+            byte[] data = Encoding.Unicode.GetBytes(s);
+            handler.Send(data);
+            handler.Shutdown(SocketShutdown.Both);
+            handler.Close();
+        }
+    }
+}
