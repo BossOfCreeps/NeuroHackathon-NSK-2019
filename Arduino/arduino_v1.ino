502d8d03917b22b12747f24426c0fd96820801fb
BossOfCreeps <seva-ra4is@mail.ru>
2019-12-09 11:20:56 +0700

    Add files via upload

---
+++ Arduino/arduino_v1.ino
@@ -0,0 +1,23 @@
+#include <Servo.h>
+
+Servo myservo;
+
+void setup() {
+  Serial.begin(9600);
+  myservo.attach(9);
+       myservo.write(90); 
+}
+
+void loop() {
+  if(Serial.available() > 0) {
+    char data = Serial.read();
+    char str[2];
+    str[0] = data;
+    if (data=='0')
+       myservo.write(100);
+    if (data=='1')
+       myservo.write(120); 
+    str[1] = '\0';
+    Serial.print(str);
+  }
+}
